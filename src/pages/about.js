f475ab8e0f181a4aeb86cfee6bbc1769092b039f
stpauljackson <stpauljackson3@gmail.com>
2023-05-08 23:14:28 +0530

    initial

---
+++ src/pages/about.js
@@ -0,0 +1,54 @@
+import { StyleSheet, View, Text, TouchableOpacity, ImageBackground } from 'react-native'
+import React from 'react'
+
+export default function About({navigation}) {
+  return (<ImageBackground  source={
+    require('./image4.png')
+
+        } style={styles.background}>
+    <View style={styles.container}>
+      <Text style={styles.heading}>This app is created as a capstone project</Text>
+      <TouchableOpacity onPress={()=>navigation.navigate('First')} style={styles.button}>
+          <Text style={styles.buttonText}>Back</Text>
+        </TouchableOpacity>
+    </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      // backgroundColor:"black",
+      // backgroundImage: require('./start.png'),
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    heading: {
+      color:"black",
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#63666A',
+        padding: 10,
+        borderRadius: 5,
+        marginHorizontal:140
+        
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign:"center"
+      },
+    message: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    background: {
+      flex: 1,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+    }
+  });
